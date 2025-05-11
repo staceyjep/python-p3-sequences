0eb54e78d2064ed213e325aae53ba76d1cd6d0d0
Stacey jepchumba <satceyjepchumbarr@gamil.com>
2025-05-11 04:07:47 +0300

    added

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = [0, 1]
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        for i in range(2, length):
+            next_fib = fib_sequence[i-1] + fib_sequence[i-2]
+            fib_sequence.append(next_fib)
+        print(fib_sequence)
+        
